e9a65c1a1cb9b50104c9cacc700021b396d93d61
Larvan2 <78135608+Larvan2@users.noreply.github.com>
2023-06-09 20:28:41 +0800

    test

--- .github/genReleaseNote.sh
+++ .github/genReleaseNote.sh
@@ -18,15 +18,15 @@ if [ -z "$version_range" ]; then
 fi
 
 echo "## What's Changed" > release.md
-git log --pretty=format:"* %s by @%an" --grep="^feat" $version_range | sort -f | uniq >> release.md
+git log --pretty=format:"* %s by @%an" --grep="^feat" --ignore-case $version_range | sort -f | uniq >> release.md
 echo "" >> release.md
 
 echo "## BUG & Fix" >> release.md
-git log --pretty=format:"* %s by @%an" --grep="^fix" $version_range | sort -f | uniq >> release.md
+git log --pretty=format:"* %s by @%an" --grep="^fix" --ignore-case $version_range | sort -f | uniq >> release.md
 echo "" >> release.md
 
 echo "## Maintenance" >> release.md
-git log --pretty=format:"* %s by @%an" --grep="^chore\|^docs" $version_range | sort -f | uniq >> release.md
+git log --pretty=format:"* %s by @%an" --grep="^chore\|^docs\|^refator" --ignore-case $version_range | sort -f | uniq >> release.md
 echo "" >> release.md
 
 echo "**Full Changelog**: https://github.com/MetaCubeX/Clash.Meta/compare/$version_range" >> release.md
